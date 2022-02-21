7994947b111183bf5b831d349e5165937053774b
Yury Sychyk <kauz@tut.by>
2022-02-21 14:40:02 +0300

    make countable

--- GM/ViewController.swift
+++ GM/ViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   var touches = 0
+    
+    
+    
+    
+    
+    
     @IBAction func buttonAction(_ sender: UIButton) {
-        
-        
-    }
+        touches += 1
+        countLabel.text = String(touches)
+        }
     
     
-    
+    @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
